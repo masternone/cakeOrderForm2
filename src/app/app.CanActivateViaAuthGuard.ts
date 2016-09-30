7ab8e7d1d9cbeb2070fd91bf1407bb2be0751086
masternone <themasternone@gmail.com>
2016-09-30 16:15:04 -0400

    Fixing the guard so it will work after a refresh

--- src/app/app.CanActivateViaAuthGuard.ts
+++ src/app/app.CanActivateViaAuthGuard.ts
@@ -1,17 +1,48 @@
 import {Injectable} from '@angular/core';
 import {CanActivate} from '@angular/router';
 import {AngularFire} from 'angularfire2';
+import {Router} from '@angular/router';
+import {Observable, AsyncSubject} from 'rxjs';
 
 @Injectable()
 export class CanActivateViaAuthGuard implements CanActivate {
-  canActivateVal: boolean = false;
+  // canActivateVal: boolean = false;
+  isLogged: AsyncSubject<any> = new AsyncSubject();
+  afAuth;
 
-  constructor(private af: AngularFire) {
-    this.af.auth.subscribe((auth) => this.canActivateVal = !!auth);
+  constructor(private af: AngularFire, private router: Router) {
+    this.afAuth = af.auth;
+    this.router;
+    this.authenticated();
   }
 
-  canActivate() {
-    console.log(this.canActivateVal);
-    return this.canActivateVal;
+  authenticated() {
+    if(this.afAuth) {
+      this.afAuth.subscribe(
+      auth => {
+        if(!!auth) {
+          this.isLogged.next(true);
+        } else {
+          this.isLogged.next(false);
+        }
+        this.isLogged.complete();
+      },
+      error => {
+        this.isLogged.next(false);
+        this.isLogged.complete();
+      }
+      )
+    } else {
+      this.isLogged.next(false);
+      this.isLogged.complete();
+    }
+  }
+
+  canActivate(): Observable<boolean> {
+    return this.isLogged.do(
+    isLoggedIn => {
+      if(!isLoggedIn) this.router.navigate([''])
+    },
+    error => console.error(error));
   }
 }
