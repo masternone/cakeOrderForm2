e18db9497074189b8f867dda3c41355361c97d54
Edward Grant <themasternone@gmail.com>
2018-01-26 12:19:02 -0500

    Fixing issue with missing Input

--- src/app/title-bar/title-bar.component.ts
+++ src/app/title-bar/title-bar.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 import {AngularFireAuth} from 'angularfire2/auth';
 
 @Component({
-             selector: 'app-title-bar',
-             templateUrl: './title-bar.component.html',
-             styleUrls: ['./title-bar.component.scss']
-           })
+  selector: 'app-title-bar',
+  templateUrl: './title-bar.component.html',
+  styleUrls: ['./title-bar.component.scss']
+})
 export class TitleBarComponent implements OnInit {
+  @Input() sidenav: Boolean;
+
   title = 'Cake Order Form 2';
 
   constructor(public afAuth: AngularFireAuth) {
